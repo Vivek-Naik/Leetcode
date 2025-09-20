c7763bf0eab868e2adad4f4b09c2c87126a9c92e
Vivek Naik <118447784+Vivek-Naik@users.noreply.github.com>
2025-09-20 23:36:35 +0530

    Time: 75 ms (8.11%) | Memory: 34.1 MB (12.82%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,8 +3,14 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> mpp;
         for(auto item :strs){
-            string key=item;
-            sort(key.begin(),key.end());
+            int count[26]={0};
+            for(char c :item){
+                count[c-'a']++;
+            }
+            string key;
+            for(int  i=0;i<26;i++){
+                key+=to_string(count[i])+'#';
+            }
             mpp[key].push_back(item);
         }
         vector<vector<string>>result;
